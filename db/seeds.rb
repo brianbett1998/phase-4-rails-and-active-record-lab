24a534b32d711cb2214a9e3fe139f876f6a7378b
BRIAN BETT <+>
2023-06-20 20:19:34 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade:90)
+Student.create(first_name: 'Brian', last_name: 'Bett', grade:70)
+Student.create(first_name: 'Vin', last_name: 'Diesel', grade:60)
